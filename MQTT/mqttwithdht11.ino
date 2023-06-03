27d8d7800a1e191b7dab842dfc8a9a5d798108a9
Iismaailx <89504513+iismaailx@users.noreply.github.com>
2023-06-03 11:25:21 +0700

    MQTT with DHT11
    tidak ada bug namun perlu di jalankan di OS linux, karena code ini memakai host dari local IP

---
+++ MQTT/mqttwithdht11.ino
@@ -0,0 +1,141 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-mqtt-publish-bme680-arduino/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define WIFI_SSID "Neng WiFi"
+#define WIFI_PASSWORD "05Aug-1977"
+#define DHTPIN D5
+// Raspberry Pi Mosquitto MQTT Broker
+#define MQTT_HOST IPAddress(192, 168, 18, 19)
+// For a cloud MQTT broker, type the domain name
+//#define MQTT_HOST "example.com"
+#define MQTT_PORT 1883
+
+#define TOPIC_TEMP "/nodemcu/data/temperature"
+#define TOPIC_HUM "/nodemcu/data/humidity"
+#define TOPIC_LED "/nodemcu/data/led"
+
+DHT dht(DHTPIN, DHT11);
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+Ticker wifiReconnectTimer;
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP& event) {
+  Serial.println("Connected to Wi-Fi.");
+  connectToMqtt();
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
+  Serial.println("Disconnected from Wi-Fi.");
+  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+  wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+  uint16_t packetIdSub = mqttClient.subscribe(TOPIC_LED, 0);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.print("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  payload: ");
+  Serial.println(payload);
+  if(payload=="ON")digitalWrite(D0,1);
+  else digitalWrite(D0,0);
+}
+
+void setup() {
+  pinMode(A0, INPUT);
+  pinMode(D0,OUTPUT);
+  Serial.begin(115200);
+  Serial.println();
+  dht.begin();
+
+
+  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  connectToWifi();
+}
+
+void loop() {
+  float newT = dht.readTemperature();
+  if (!isnan(newT)) {
+    uint16_t packetIdPub1 = mqttClient.publish(TOPIC_TEMP, 0, false, String(newT).c_str());
+    Serial.println(newT);
+  }
+  delay(1000);
+  float newH = dht.readHumidity();
+  if (!isnan(newH)) {
+    uint16_t packetIdPub1 = mqttClient.publish(TOPIC_HUM, 0, false, String(newH).c_str());
+    Serial.println(newH);
+  }
+  delay(1000);
+
+}
